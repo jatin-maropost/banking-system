8dec67f68092f5b980c0139f3c33ee18b56985d6
Jatin Kamboj <jatin.kamboj@maropost.com>
2021-10-17 22:46:17 +0530

    Added cli handler for actions to be performed by the user

---
+++ lib/banking_system/cli.rb
@@ -0,0 +1,29 @@
+class BankingSystem::CLI
+
+    def initialize
+        # BankingSystem::Database.new
+        @prompt = TTY::Prompt.new
+        @auth = BankingSystem::Auth.new
+    end
+
+    def call
+        @action = @prompt.select("What would you like to do?", %w(Login Signup Exit))
+        perform_action
+    end
+
+    def perform_action
+         case @action
+         when "Login"
+            @auth.login
+         when "Signup"
+            @auth.signup
+         else
+             puts "Exit"
+         end
+    end
+
+    def exit
+        puts "Exit the program"
+    end
+
+end
